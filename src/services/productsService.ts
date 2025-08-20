9fed81f032080b91fdb8e42f0eb540898a0f2a0c
Angela Carvalho <angelalopo@outlook.com>
2025-08-20 20:45:05 -0300

    refactor: update product retrieval to use API and add tests with nock

--- src/services/productsService.ts
+++ src/services/productsService.ts
@@ -1,15 +1,16 @@
-import { products } from "../productsData";
-
 type Products = {
     id: string;
     name: string;
     price: number;
 };
 
-export const getProducts = (): Products[] => {
-    return products.map(product => ({
+export const getProductsApi = async (): Promise<Products[]> => {
+    const response = await fetch('https://pretalab-api-439254010866.us-central1.run.app/products');
+    const result: { data: Products[] } = await response.json();
+
+    return result.data.map((product: Products) => ({
         id: String(product.id),
         name: product.name,
         price: product.price
     }));
-};
+}
--- tests/integration/getProductsIntegration.spec.ts
+++ tests/integration/getProductsIntegration.spec.ts
@@ -1,8 +1,18 @@
 import request from "supertest"; 
 import app from "../../src/app";
+import nock from "nock";
 
 describe("Get Products", () => {
     it("should return a list of products", async () => {
+        nock("https://pretalab-api-439254010866.us-central1.run.app")
+            .get("/products")
+            .reply(200, {
+                data: [
+                    { id: 1, name: "Product 1", price: 100 },
+                    { id: 2, name: "Product 2", price: 200 }
+                ]
+            });
+
         const response = await request(app)
             .get("/products");
 
